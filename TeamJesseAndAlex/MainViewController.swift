5c44e448ade3785862badc9b28b5a57e85074e2c
Brian Salisbury <briansalisbury@Brians-MacBook-Pro.local>
2019-01-18 09:43:03 -0600

    Initial commit.

---
+++ TeamJesseAndAlex/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  TeamJesseAndAlex
+//
+//  Created by Brian Salisbury on 1/18/19.
+//  Copyright © 2019 TOM Vanderbilt 2019. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
